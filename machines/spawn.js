4f140d0d24d258bba46a4ea2d3183eb934340fbb
Mike McNeil <mike@balderdash.co>
2015-05-08 09:40:31 -0500

    trivial

--- machines/spawn.js
+++ machines/spawn.js
@@ -42,7 +42,7 @@ module.exports = {
     },
 
     success: {
-      variableName: 'output',
+      variableName: 'bufferedOutput',
       description: 'Done.',
       example: {
         stdout: '...',
