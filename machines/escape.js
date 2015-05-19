65975b437162af653520f47385170ae4fac6e134
Mike McNeil <mike@balderdash.co>
2015-05-19 08:41:49 -0500

    .escape() machine (for cli opts)

---
+++ machines/escape.js
@@ -0,0 +1,71 @@
+module.exports = {
+
+
+  friendlyName: 'Escape',
+
+
+  description: 'Escape a value for use as a CLI option.',
+
+
+  extendedDescription: 'The result should be wrapped in single quotes when building up the final CLI options string.  For instance `--foo=\'RESULT_FROM_THIS_MACHINE_HERE\'`  or `-f \'RESULT_FROM_THIS_MACHINE_HERE\'',
+
+
+  cacheable: true,
+
+
+  sync: true,
+
+
+  inputs: {
+
+    value: {
+      friendlyName: 'Value',
+      description: 'The value to escape as a CLI option.',
+      example: '*',
+      required: true
+    },
+
+  },
+
+
+  exits: {
+
+    couldNotSerialize: {
+      description: 'The provided value could not be serialized into a JSON string.'
+    },
+
+    success: {
+      variableName: 'escaped',
+      example: '{"foo":"bar"}',
+      description: 'Done.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    var _ = require('lodash');
+    var MPJSON = require('machinepack-json');
+
+    // Stringify if necessary
+    var val = inputs.value;
+    if (_.isObject(val)) {
+      val = MPJSON.stringifySafe({value:val}).execSync();
+    }
+    else {
+      val = val+'';
+    }
+    if (!_.isString(val)) {
+      return exits.couldNotSerialize();
+    }
+
+    // Now escape the resulting string
+    val = val.replace(/'/g,'\'\\\'\'');
+
+    return exits.success(val);
+  },
+
+
+
+};
