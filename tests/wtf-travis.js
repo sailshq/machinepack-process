b758be27cbfac8cfdbe0136435036c4e09c2fe7d
Mike McNeil <mike@balderdash.co>
2016-08-26 13:09:39 -0500

    Wulp it was my bad-- the issue was some kind of conflict in my local node_modules folder.

--- tests/wtf-travis.js
+++
@@ -1,8 +0,0 @@
-var EXPECTED_TMM_SEMVER_RANGE = require('../package.json').devDependencies['test-machinepack-mocha'];
-console.log('hey. travis ci.  what is going on?', 'package.json says to use TMM@'+EXPECTED_TMM_SEMVER_RANGE+'.');
-
-
-console.log('What version of TMM are you ACTUALLY using?',require('test-machinepack-mocha/package.json').version);
-
-
-console.log('What version of test-mp is THAT one ACTUALLY using?',require('test-machinepack-mocha/node_modules/test-machinepack/package.json').version);
